8864ae8873bfaf4d9f19f20a1e4b827437ddd56e
Fuad Khan <107122588+Fuad-Khan@users.noreply.github.com>
2023-01-27 02:08:33 +0600

    Add files via upload

---
+++ First.java
@@ -0,0 +1,14 @@
+
+package Java;
+
+public class First 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Fuad Khan");
+        System.out.print("Fuad Khan");
+        
+        
+    }
+    
+}
